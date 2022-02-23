69ee6efed7e4ddf0fde7b64b49945731d87fcc64
neila <78797625+neila-a@users.noreply.github.com>
2022-02-23 20:11:58 +0800

    Update toolbox.js

--- toolbox.js
+++ toolbox.js
@@ -1,12 +1,11 @@
 /*             Menu             */
-/* 1. printf ---------- line 11 */
-/* 2. loadbootstrap --- line 16 */
-/* 3. alertbox -------- line 21 */
-/* 4. getURLparameter - line 32 */
-/* 5. importScripts --- line 44 */
-/* 6. importCss ------- line 78 */
-/* 7. replaceAll ------ line 83 */
-/* 8. HyperMD --------- line 90 */
+/* 1. printf ---------- line 10 */
+/* 2. loadbootstrap --- line 15 */
+/* 3. getURLparameter - line 20 */
+/* 4. importScripts --- line 32 */
+/* 5. importCss ------- line 66 */
+/* 6. replaceAll ------ line 71 */
+/* 7. HyperMD --------- line 78 */
 
 function printf(string) {
 	var inbody = document.body.innerHTML;
@@ -16,17 +15,6 @@ function printf(string) {
 function loadbootstrap() {
 	var inhead = document.head.innerHTML;
 	document.head.innerHTML = inhead + '<!-- 最新版本的 Bootstrap 核心 CSS 文件 --><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"><!-- 可选的 Bootstrap 主题文件（一般不用引入） --><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous"><!-- 最新的 Bootstrap 核心 JavaScript 文件 --><script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>';
-}
-
-class alertbox {
-	/* ES2015 or UP */
-	constructor(title, word, type) {
-		// type: success | danger | info | warning
-		loadbootstrap();
-		var id = Math.random() * 1000000000;
-		var inbody = document.body.innerHTML;
-		document.body.innerHTML = '<div class="container"><div class="row clearfix"><div class="col-md-12 column"><div class="alert alert-' + type + ' alert-dismissable" id="' + id + '"><button type="button" class="close" data-dismiss="alert" aria-hidden="true" onclick="document.getElementById(' + id + ').style.display = \'none\';">×</button><h4>' + title + '</h4>' + word + '</div></div></div></div>' + inbody;
-	}
 }
 
 function getURLparameter(variable) {
