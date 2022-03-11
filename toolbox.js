35b51627b3fb42e51e437aa43886a5a0001227b8
neila <78797625+neila-a@users.noreply.github.com>
2022-03-11 20:23:15 +0800

    Update toolbox.js

--- toolbox.js
+++ toolbox.js
@@ -1,26 +1,13 @@
 /*             Menu           */
-/* 1. printf -------- line 10 */
-/* 2. loadbootstrap - line 15 */
-/* 3. _GET ---------- line 20 */
-/* 4. importScripts - line 32 */
-/* 5. importCss ----- line 66 */
-/* 6. replaceAll ---- line 71 */
-/* 7. HyperMD ------- line 78 */
+/* 1. printf -------- line 08 */
+/* 2. importScripts - line 13 */
+/* 3. importCss ----- line 47 */
+/* 4. replaceAll ---- line 52 */
+/* 5. HyperMD ------- line 59 */
 
 function printf(string) {
 	var inbody = document.body.innerHTML;
 	document.body.innerHTML = inbody + string;
-}
-
-function loadbootstrap() {
-	var inhead = document.head.innerHTML;
-	document.head.innerHTML = inhead + '<!-- 最新版本的 Bootstrap 核心 CSS 文件 --><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"><!-- 可选的 Bootstrap 主题文件（一般不用引入） --><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous"><!-- 最新的 Bootstrap 核心 JavaScript 文件 --><script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>';
-}
-
-var query = window.location.search.substring(1);
-var vars = query.split("&");
-for (var i = 0; i < vars.length; i++) {
-	var _GET = vars[i].split("=");
 }
 
 if (!importScripts) {
@@ -70,7 +57,7 @@ String.prototype.replaceAll = function (s1, s2) {
 }
 
 importScripts('https://cdn.jsdelivr.net/npm/marked/marked.min.js');
-for (var mdes = document.getElementByTagName("markdown");1=1;;)
-{
+var mdes = document.getElementByTagName("markdown");
+for (var i = 0;i < mdes.length;i++;) {
     mdes[i].innerHTML = marked.parse(mdes[i].innerHTML);
 }
