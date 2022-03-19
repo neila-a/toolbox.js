9a3c84e7b2dc20f29485bdc615e0a3386400abe5
neila <78797625+neila-a@users.noreply.github.com>
2022-03-19 11:53:50 +0800

    Update toolbox.js

--- toolbox.js
+++ toolbox.js
@@ -1,12 +1,12 @@
 function printf(string) {				/*                Menu              */
-	var inbody = document.body.innerHTML;		/* 1. printf -------------- line 01 */
-	document.body.innerHTML = inbody + string;	/* 2. importScripts ------- line 06 */
-}							/* 3. importCss ----------- line 40 */
-							/* 4. replaceAll ---------- line 45 */
-if (!importScripts) {					/* 5. $() ----------------- line 52 */
-	var importScripts = (function (globalEval) {	/* 6. Function.bind ------- line 56 */
-		var xhr = new XMLHttpRequest;		/* 7. new DocumentElement - line 74 */
-		return function importScripts() {
+	var inbody = document.body.innerHTML;		/* 1. printf -------------- line 001 */
+	document.body.innerHTML = inbody + string;	/* 2. importScripts ------- line 006 */
+}							/* 3. importCss ----------- line 040 */
+							/* 4. replaceAll ---------- line 045 */
+if (!importScripts) {					/* 5. $() ----------------- line 052 */
+	var importScripts = (function (globalEval) {	/* 6. Function.bind ------- line 056 */
+		var xhr = new XMLHttpRequest;		/* 7. vis(原vis.js) ------- line 074 */
+		return function importScripts() {       /* 8. new DocumentElement - line 121 */
 			var
 				args = Array.prototype.slice.call(arguments),
 				len = args.length,
@@ -70,6 +70,53 @@ Function.prototype.bind = Function.prototype.bind || function(context){
 	bound.prototype = new F();
 	return bound;
 }
+
+const vis = {
+    init: function () {
+        String.prototype.delete = function (deletestring) {
+            var r = this.replace(deletestring, "");
+            return r;
+        }
+        var dbh_vis = document.head.innerHTML;
+        var dbi_vis = document.body.innerHTML;
+        if (vis.data.map == "") {   
+            vis.data.map = document.documentElement.outerHTML;
+            if (document.compatMode == "CSS1Compat") {
+            	vis.data.map = `<!DOCTYPE html>` + document.documentElement.outerHTML;
+            }
+            var parser_vis = new DOMParser();
+            window.vdom = parser_vis.parseFromString(vis.data.map, "text/html");
+            vdom.render = function () {
+                document.open();
+                document.write(vdom.documentElement.outerHTML);
+            };
+        }
+        var regin_vis = /{{.*}}/g;
+        function rep_vis(varname_vis) {
+            /* a表示正则匹配到的内容  b表示大括号里的内容  c表示匹配到的内容出现的位置 */
+            var ro_vis = varname_vis.delete("{{").delete("}}").delete(/ /g);
+            try {
+                var r_vis = eval(ro_vis);
+                return r_vis;
+            } catch (err) {
+                console.error(`Vis Error: ${err}`);
+                return varname_vis;
+            }
+        }
+        document.body.innerHTML = dbi_vis.replace(regin_vis, rep_vis);
+        document.head.innerHTML = dbh_vis.replace(regin_vis, rep_vis);
+        var ifels_vis = document.getElementsByTagName("if");
+        for (var i = 0;i<ifels_vis.length;i++) {
+            try {
+                eval(`if (${ifels_vis[i].getAttribute("cond")}) {ifels_vis[i].innerHTML = ifels_vis[i].innerHTML.delete("<!--").delete("-->");} else {ifels_vis[i].innerHTML = "<!--${ifels_vis[i].innerHTML.replace(/<!--.*-->/g, '')}-->";}`);
+            } catch {}
+        }
+    },
+    data: {
+        map: ""
+    }
+}
+vis.init();
 
 function DocumentElement(tagname) {
 return document.createElement(tagname);
